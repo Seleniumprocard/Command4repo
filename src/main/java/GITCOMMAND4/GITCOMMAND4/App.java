4c39245209efc99a422c6605a05c8108ace2b5e5
Seleniumprocard <wehatemails@gmail.com>
2023-11-18 18:20:32 +0530

    first commit java1

---
+++ src/main/java/GITCOMMAND4/GITCOMMAND4/App.java
@@ -0,0 +1,13 @@
+package GITCOMMAND4.GITCOMMAND4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Gitcommand1/java1.java
@@ -0,0 +1,5 @@
+package Gitcommand1;
+
+public class java1 {
+
+}
